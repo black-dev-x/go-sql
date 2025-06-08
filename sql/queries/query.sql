d1ad2cf0ddd8e6a4716f563ebf03eb79e8be88b8
Thiago <thiago.dev.x@gmail.com>
2025-06-08 14:07:33 -0300

    create category

--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -4,3 +4,5 @@ SELECT * FROM categories;
 -- name: GetCategory :one
 SELECT * FROM categories WHERE id = ?;
 
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name, description) VALUES (?, ?, ?);
